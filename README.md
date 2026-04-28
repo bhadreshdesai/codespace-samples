b164cb014d173080388288691df5a257f72891b9
Bhadresh Desai <16066580+bhadreshdesai@users.noreply.github.com>
2026-04-28 18:50:19 +0000

    fix devcontainer db port access issue

--- README.md
+++ README.md
@@ -37,3 +37,15 @@ use .env environment variables for docker-compose and rust app
 ```shell
 cargo add dotenvy
 ```
+
+### step 5
+fix devcontainer issue by using `dockerComposeFile` option in [devcontainer.json](./.devcontainer/devcontainer.json)
+
+`dockerComposeFile` uses an array of docker-compose.yaml files
+
+../docker-compose.yaml is first in the list, so the project root becomes the current working director
+
+.env is also picked up correctly from the project root
+
+Note: it takes about 45 seconds to access the pgadmin. Look for Starting gunicorn 23.0.0 and Listening at: http://[::]:5050 (1)
+
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,19 @@ use std::env;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Silently ignore if .env is missing
     dotenv().ok();
+    let pghost = env::var("POSTGRES_HOST").unwrap_or_else(|_| "localhost".to_string());
     let pgdbname = env::var("POSTGRES_DB").unwrap_or_else(|_| "pgdb".to_string());
     let pguser = env::var("POSTGRES_USER").unwrap_or_else(|_| "pguser".to_string());
     let pgpwd = env::var("POSTGRES_PASSWORD").unwrap_or_else(|_| "pgpwd".to_string());
-    let connect_params = format!("host=localhost dbname={} user={} password={}", pgdbname, pguser, pgpwd);
+    let connect_params = format!(
+        "host={} dbname={} user={} password={}",
+        pghost, pgdbname, pguser, pgpwd
+    );
+
+    println!("Connecting to database with params: {}", connect_params);
 
     // 1. Establish connection
-    let mut client = Client::connect(
-        &connect_params,
-        NoTls,
-    )?;
+    let mut client = Client::connect(&connect_params, NoTls)?;
 
     // 2. Execute a query
     for row in client.query("SELECT 1 + 1", &[])? {
